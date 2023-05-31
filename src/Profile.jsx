1a9a37aa09c117c0572197685e0213a85355d3de
Evan Oreschnick <evan.oreschnick.1211@outlook.com>
2023-05-30 21:09:50 -0500

    Updated Profile and Wrote Agenda

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,23 +1,12 @@
  import { useState,useRef,useEffect,useContext } from "react";
  import AuthContext from "./AuthContext";
  import { silentJSON, processAlert } from "./FetchRoutines";
- //import { handleLogin, handleNewAccount } from "./Home";
  function Profile() {
 
-     //useEffect(() => {getProfile()},[]);
-     let nameInput = useRef();
-     let passInput = useRef();
-     let fullName = useRef();
-
     const jwt = useContext(AuthContext);
-    const [user,setProfile] = useState();
+
     const [events, setEvents ] = useState([]);
 
-    // function getProfile() {
-    //     const headers = {"Authorization" : "Bearer "+jwt};
-    //     fetch("http://localhost:8085/user", {method:"GET",headers:headers}).then(silentJSON)
-    //         .then(response=>{setProfile(response)});
-    // }
 
     function userActivities(){
         const headers = {"Authorization" : "Bearer "+jwt};
@@ -36,7 +25,7 @@
                 {events.map((activity, index) => (
                     <div key= {index}>
                         <h3>{activity.title}</h3>
-                        <p>{activity.description}</p>
+                        <p>Description: {activity.description}</p>
                         <p>Start: {activity.start}</p>
                         <p>End: {activity.end} </p>
                     </div>
--- src/agenda.jsx
+++ src/agenda.jsx
@@ -1,6 +1,34 @@
-
+import { useEffect, useState } from 'react'
+import { silentJSON } from "./FetchRoutines"
+import DateTimePicker from 'react-datetimepicker';
 function Agenda(){
 //create a table and for loop through the datatabse to create a new line for each value in datbase and display events throughout the week!
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        fetchAgenda();
+    }, []);
+
+    function fetchAgenda(){
+        fetch("http://localhost:8085/activity")
+        .then(silentJSON)
+        .then(response => setEvents(response));nq
+    }
+
+    return (
+        <div>
+            <h4>All Posted Activities</h4>
+            {events.map(activity =>(
+                <div key={activity.id}>
+                    <h5>{activity.title}</h5>
+                    <p>Description: {activity.description}</p>
+                    <p>Start: {activity.start}</p>
+                    <p>End: {activity.end}</p>
+                 </div>   
+            ))}
+        </div>
+    );
+
 
 }
 export default Agenda;
